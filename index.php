52fe15c2f9f6003c54c9e93890f1fab7ccb1c5d3
Santos1 <global.santos1@gmail.com>
2013-07-17 10:20:12 -0700

    Clean Template

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+	<?php include 'header.php'?>
+
+	<body>
+		<?php include 'navigation.php'?>
+		<div id="PageContainerOuter">
+			<div id="PageContainer">
+				<?php //include 'info1.php';?>
+			</div>
+		</div>
+	</body>
+</html>
---
+++ navigation.php
@@ -0,0 +1,137 @@
+<div id="HeaderContainer">
+	<div id="HeaderContent">
+		<div id="Branding">
+			<div style="float: left; height: 29px;">
+				<a href="#">
+					<div style="position: relative; left: 17px; float: left; height: 29px;">
+						<img src="images/NeuroMama_small.png" alt="NEUROMAMA" height="31" width="108">
+					</div>
+				</a>
+				<span style="position: relative; left: 25px; font-family: 'Didact Gothic', sans-serif; font-size: 18px; color: #ffffff">The 21st Century Search Engine, Based on Neuro Technology.</span>
+			</div>
+		</div>
+		
+		<!-- Navigation Items -->
+		<div id="HeaderNavigation">
+			<!-- Tabs Panel -->
+			<ul id="nav">
+			
+				<li onmouseover=painttab(1); onmouseout=unpainttab(1);><a href="#" onclick="return false;" class="btn-neuinf">Home</a>
+					<ul onmouseover=painttab(1); onmouseout=unpainttab(1);>
+						<li style="padding-top: 8px;"><a href="#" >About NeuroMama</a></li>
+						<li style="padding-top: 8px;"><a href="#" >Corporate documents</a></li>
+						<li style="padding-top: 8px; padding-bottom: 5px;"><a href="#" >Contact Us</a></li>
+					</ul> 
+				</li> 
+				
+				
+				<li onmouseover=painttab(2); onmouseout=unpainttab(2);><a href="#" onclick="return false;" class="btn-neuvid">Press Center  Investor relations  Charity</a>
+					<ul onmouseover=painttab(2); onmouseout=unpainttab(2);>
+						<li style="padding-top: 8px;"><a href="#" >Stock Information (8-K)</a></li>
+						<li style="padding-top: 8px;"><a href="#" >Financial Releases</a></li>
+						<li style="padding-top: 8px;"><a href="#" >Important Events</a></li>
+						<li style="padding-top: 8px;"><a href="#" >Disclaimers</a></li>
+						<li style="padding-top: 8px;"><a href="#" >Subscribe</a></li>
+						<li style="padding-top: 8px; padding-bottom: 5px;"><a href="#" >Contact Us</a></li>
+					</ul> 
+				</li> 
+				
+				
+				<li onmouseover=painttab(3); onmouseout=unpainttab(3);><a href="#" onclick="return false;" class="btn-neutab1">Management</a>
+					<ul onmouseover=painttab(3); onmouseout=unpainttab(3);>
+						<li style="padding-top: 8px;"><a href="#" >Press Attache</a></li>
+						<li style="padding-top: 8px;"><a href="#" >Lastest News</a></li>
+						<li style="padding-top: 8px;"><a href="#" >Press Releases</a></li>
+						<li style="padding-top: 8px;"><a href="#" >Write for Us</a></li>
+						<li style="padding-top: 8px;"><a href="#" >Subscribe</a></li>
+						<li style="padding-top: 8px; padding-bottom: 5px;"><a href="#" >Contact Us</a></li>
+					</ul> 
+				</li> 
+				
+				
+				<li onmouseover=painttab(4); onmouseout=unpainttab(4);><a href="#" onclick="return false;" class="btn-neutab2">Products</a>
+					<ul onmouseover=painttab(4); onmouseout=unpainttab(4);>
+					    <li style="padding-top: 8px;"><a href="#" >Aims & Principles</a></li>
+					    <li style="padding-top: 8px;"><a href="#" >Upcoming events</a></li>
+					    <li style="padding-top: 8px;"><a href="#" >Latest Actions</a></li>
+					    <li style="padding-top: 8px;"><a href="#" >Donate Event</a></li>
+					    <li style="padding-top: 8px;"><a href="#" >Cooperation</a></li>
+					    <li style="padding-top: 8px; padding-bottom: 5px;"><a href="#" >Contact Us</a></li>
+					</ul> 
+				</li> 
+				
+				
+				<li onmouseover=painttab(5); onmouseout=unpainttab(5);><a href="#" onclick="return false;" class="btn-neutab3">Projects</a>
+					<ul onmouseover=painttab(5); onmouseout=unpainttab(5);>
+						<li style="padding-top: 8px;"><a href="#" >Corporate Structure</a></li>
+						<li style="padding-top: 8px;"><a href="#" >Management</a></li>
+						<li style="padding-top: 8px; padding-bottom: 5px;"><a href="#" >Contact Us</a></li>
+					</ul> 
+				</li> 
+				
+				
+				<li onmouseover=painttab(6); onmouseout=unpainttab(6);><a href="#" onclick="return false;" class="btn-neusmart1">Technologies</a>
+					<ul onmouseover=painttab(6); onmouseout=unpainttab(6);>
+					    <li style="padding-top: 8px; "><a href="#" >NeuroMail</a></li>
+					    <li style="padding-top: 8px; "><a href="#" >NeuroZone </a></li>
+					    <li style="padding-top: 8px; "><a href="#" >NeuroPlatform </a></li>
+					    <li style="padding-top: 8px; "><a href="#" >NeuroConnect </a></li>
+					    <li style="padding-top: 8px; "><a href="#" >NeuroWorld </a></li>
+					    <li style="padding-top: 8px; "><a href="#" >NeuroArchives </a></li>
+					    <li style="padding-top: 8px; "><a href="#" >NeuroSphere </a></li>
+					    <li style="padding-top: 8px; "><a href="#" >NeuroMama Vacation Club</a></li>
+					    <li style="padding-top: 8px; "><a href="#" >NeuroMediaCenter </a></li>
+					    <li style="padding-top: 8px; "><a href="#" >NeuroStudio </a></li>
+					    <li style="padding-top: 8px; "><a href="#" >NeuroImages </a></li>
+					    <li style="padding-top: 8px; "><a href="#" >NeuroBot </a></li>
+					    <li style="padding-top: 8px; "><a href="#" >NeuroPay </a></li>
+					    <li style="padding-top: 8px; "><a href="#" >NeuroDollars </a></li>
+					    <li style="padding-top: 8px; "><a href="#" >NeuroPoints </a></li>
+					    <li style="padding-top: 8px; "><a href="#" >NeuroPad </a></li>
+					    <li style="padding-top: 8px; "><a href="#" >NeuroPhone </a></li>
+					    <li style="padding-top: 8px; padding-bottom: 5px;"><a href="#" >NeuroCertificate </a></li>
+					</ul> 
+				</li> 
+				
+				
+				<li onmouseover=painttab(7); onmouseout=unpainttab(7);><a href="#" onclick="return false;" class="btn-neusmart2">Advertisement</a>
+					<ul onmouseover=painttab(7); onmouseout=unpainttab(7);>
+						<li style="padding-top: 8px; "><a href="#">Search Engine</a></li>
+						<li style="padding-top: 8px; "><a href="#">Neural Technology</a></li>
+						<li style="padding-top: 8px; "><a href="#">FSRewards</a></li>
+						<li style="padding-top: 8px; "><a href="#">SMSA Search Alternative</a></li>
+						<li style="padding-top: 8px; "><a href="#">PPC Dutch Auction</a></li>
+						<li style="padding-top: 8px; "><a href="#">Subcribe</a></li>
+						<li style="padding-top: 8px; padding-bottom: 5px;"><a href="#">Contact Us</a></li>
+					</ul> 
+				</li> 
+				
+				
+				<li onmouseover=painttab(8); onmouseout=unpainttab(8);><a href="#" onclick="return false;" class="btn-neuvac1">Rules & Principles</a>
+					<ul onmouseover=painttab(8); onmouseout=unpainttab(8);>
+						<li style="padding-top: 8px;"><a href="#" >Ads Policy</a></li>
+						<li style="padding-top: 8px;"><a href="#" >Offer Ads Space</a></li>
+						<li style="padding-top: 8px;"><a href="#" >Buy Ads Space</a></li>
+						<li style="padding-top: 8px;"><a href="#" >Ads Specifications</a></li>
+						<li style="padding-top: 8px; padding-bottom: 5px;"><a href="#" >Contact Us</a></li>
+					</ul> 
+				</li> 
+				
+				
+				<li onmouseover=painttab(9); onmouseout=unpainttab(9);><a href="#" onclick="return false;" class="btn-neuvac2">Contacts</a>
+					<ul onmouseover=painttab(9); onmouseout=unpainttab(9);>
+						<li style="padding-top: 8px;"><a href="#" >Adult Content </a></li>
+						<li style="padding-top: 8px;"><a href="#" >User Confidentiality</a></li>
+						<li style="padding-top: 8px;"><a href="#" >Software Distribution</a></li>
+						<li style="padding-top: 8px;"><a href="#" >Media Distribution</a></li>
+						<li style="padding-top: 8px;"><a href="#" >Terms & Conditions</a></li>
+						<li style="padding-top: 8px;"><a href="#" >Private Policy</a></li>
+						<li style="padding-top: 8px; padding-bottom: 5px;"><a href="#" >Copyrights</a></li>
+					</ul> 
+				</li> 
+			</ul>
+		</div>
+		<div id="SubHeader"><span style="font-family: 'Didact Gothic',sans-serif; font-size: 13px; color: #fff"><strong style="font-size: 16px;">Fewer duplicates, more accurate returns, faster results, Frequent Searcher Rewards for gift certificates. Join now!</strong></span></div>
+		<!-- /HeaderNavigation -->
+	</div>
+</div>
