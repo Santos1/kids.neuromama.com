7d78360cdaf290621d28dabaa0f6919098f85e75
Rocandio <juanrocandio@hotmail.com>
2013-07-18 15:39:43 -0700

    Adding images

--- images_banner.php
+++ images_banner.php
@@ -1,6 +1,6 @@
 <?php
-# number of images (12)
-	$images = rand(1,12);
+# number of images (13)
+	$images = rand(1,13);
 ?>
 <div style="position: relative; overflow:Hidden; width: 1015px; height: 101px;">
 	<img id="img_banner" src="images/banners/<?php echo $images;?>.jpg" alt="" style="position: absolute; top: 0; left: 0;" />
--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 <html>
-	<?php include 'header.php';?>
+	<?php
+		
+		include 'header.php';?>
 
 	<body>
 		<?php include 'navigation.php';?>
 		<div id="PageContainerOuter">
 			<div id="PageContainer">
+				<?php
+					include 'images_banner.php'; 
+				?>
 				<?php #include 'info1.php';?>
 			</div>
 		</div>
